286aab382cba07d87901857b417a1e6e6b5c0d26
Fabio Vivian <f.vivian@hotmail.com>
2018-07-04 17:28:16 +0200

    add createVid script
    add the script used to generate the animated gif. update the Terra Mater python script to include /timelapse command and the subsequent necessities

---
+++ createVid.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Fabio Vivian (fabio.vivian@esa.int), based on the work of Antonio Cuomo
+# Simple Bot to reply to Telegram location sharing with related Earth Observation images
+# This program is made available  under the CC BY 2.0 license.
+"""
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+"""
+
+import requests
+import cv2
+import pickle
+import logging
+import numpy as np
+import time
+import json
+from pyproj import Proj, transform
+import io
+from PIL import Image
+import os
+
+userOld = None
+timeOld = None
+
+logformat = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=logformat,
+                    filename=f'vidScript.log',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+logger.info(f'Starting the bot ...')
+
+def sendVid(fileID, dictIn):
+    
+    sat  = dictIn['sat']
+    lon, lat = dictIn['location']
+
+    try:
+        vidDataRaw = getVidData(sat, lon, lat, fileID)
+    except:
+        logger.info(f'Could not get Video Data getVidData(sat, lon, lat, fileID).')
+        return
+    if len(vidDataRaw) <= 5:
+        os.rename(f'in/{fileID}', f'in/{fileID}EMPTY')
+        logger.info('amount of usable images too low')
+        return
+    # invert data to BGR for openCV
+    vidDataList = []
+    for i in range(len(vidDataRaw)):
+        vidDataList.append(vidDataRaw[i][1][:,:,::-1])
+    vidData = np.array(vidDataList)
+
+    fourcc = cv2.VideoWriter_fourcc('H','2','6','4')
+    video = None
+    fps = 25
+    filename = f'out/{fileID}.mp4' # XVID / avi works on phone
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontSize = 0.3
+    color = (255,255,255)
+    thickness = 1
+    linetype = 0
+    
+    i=0
+    starting = True
+    prev_frame = np.uint8([250])
+    
+    video = cv2.VideoWriter(filename, fourcc, fps, (480,320))
+    logger.info('entering video creation loop')
+    while(True):
+
+        frame = vidData[i]
+        
+        #cv2.rectangle(vidData[i],(10,20),(150,40),(255,255,255),60)
+        cv2.rectangle(vidData[i], (0,0),(80,50),(1,1,1),
+                      thickness=-1, lineType=8, shift=0)
+        # putText(image as array, text as string, position in the image, ..., ..., color as bgr?, ., .)
+        cv2.putText(frame, vidDataRaw[i][0]      ,(10,15), font, fontSize, color, thickness, linetype)
+        cv2.putText(frame, f'lon: {"%.2f" % lon}',(10,30), font, fontSize, color, thickness, linetype)
+        cv2.putText(frame, f'lat: {"%.2f" % lat}',(10,45), font, fontSize, color, thickness, linetype)
+          
+        if starting==True:
+            prev_frame = frame
+            starting = False
+            for m in range(int(round(fps/2.))):
+                video.write(frame)
+        else:
+            for l in range(1,20):
+                weight = (20-l)/20
+                #get the blended frames in between
+                mid_frame = cv2.addWeighted(prev_frame,weight,frame,1-weight,0)
+                video.write(mid_frame)
+
+            for m in range(int(round(fps/2.))):
+                # add half a second, filled with unprocessed scenes between the blended frames
+                video.write(frame)
+            prev_frame = frame
+        i += 1
+        if i == len(vidData):
+            break
+
+
+    video.release()
+    try:
+        os.rename(filename, f'out/{fileID}DONE.mp4')
+        logger.info(f'timelapse video saved as out/{fileID}DONE.mp4')
+    except Exception as e:
+        logger.info(f'could not save file {fileID} because of the error: {e}')
+    
+    return
+    
+def getVidInfo(sat, lon, lat, fileID):
+        
+        
+    params_eocurl = {'dataset': 'ESA-DATASET',
+                     'lat': f'{lat}',
+                     'lon': f'{lon}',
+                     'maxRecords': 100,
+                     'sortOrder': 'descending',
+                     'sortParam': 'startDate'}
+    
+    if sat == 'S2':
+        satellite = 'Sentinel2'
+        params_eocurl['cloudCover'] = '[0,10]'
+        satTimeout = 10
+    elif sat == 'S3':
+        satellite = 'Sentinel3'
+        params_eocurl['processingLevel'] = 'LEVEL1'
+        satTimeout = 40
+    
+    EOCURL = f'https://finder.eocloud.eu/resto/api/collections/{satellite}/search.json?'
+    
+    try:
+        r1 = requests.get(EOCURL, params_eocurl, timeout=satTimeout)
+        logger.info(f'Finder responds in {r1.elapsed.total_seconds()} seconds to {r1.url}.')
+    except Exception as e:
+        import urllib.parse as up
+        url = EOCURL + up.urlencode(params_eocurl)
+        logger.info(f'Finder did not respond in time {satTimeout}, {e}. URL = {url}')
+        os.rename(f'in/{fileID}', f'in/{fileID}TIMEDOUT')
+        return
+    js = json.loads(r1.content)
+    products = []
+    day_old = None
+    
+    try:
+        for j in js['features']:
+            day = j['properties']['startDate'].split('T')[0]
+            if day_old != day:
+                products.append((day, j))
+            day_old = day
+            #if len(products) >= 10:
+            #    return(list(reversed(products)))
+        if len(products)< 10:
+            logger.info(f'Not enough dates. Number of dates: {len(products)}. '
+                        f'URL: {r1.url}')
+            return(list(reversed(products)))
+    except Exception as e:
+        logger.info(f'Could not get the dates from the EO Cloud. {e}')
+
+    return(list(reversed(products)))
+
+def getVidData(sat, lon, lat, fileID):
+    
+    vidDates = getVidInfo(sat, lon, lat, fileID)
+    
+    params = {'service': 'WMS',
+              'request': 'GetMap',
+              'styles': '',
+              'format': 'image/png',
+              'version': '1.1.1',
+              'showlogo': 'false',
+              'height': 320,
+              'width': 480,
+              'srs': 'EPSG%3A3857'}
+    
+    if sat == 'S2':
+        ID = 'c539afcc-bc6d-46e9-9724-5a971dd62e3f'
+        URL = 'https://services.sentinel-hub.com/ogc/wms/'+ID
+        params['layers'] = 'S2-TRUE-COLOR'
+        reso = 60
+        threshold = 5 # in percent, 100% means every image will be used.
+    elif sat == 'S3':
+        ID = '2c05911c-1db9-43c1-80ef-6796c9fd03dd'
+        URL = 'http://services.eocloud.sentinel-hub.com/v1/wms/'+ID
+        params['layers'] = 'S3_TRUE_COLOR'
+        threshold = 75 # in percent, 100% means every image will be used.
+        reso = 1000
+        
+    inProj = Proj(init='epsg:4326')
+    outProj = Proj(init='epsg:3857')
+    xC,yC = transform(inProj,outProj, lon, lat)
+    xmin = xC - 480*reso/2
+    xmax = xC + 480*reso/2
+    ymin = yC - 320*reso/2
+    ymax = yC + 320*reso/2
+
+    params['bbox'] = str(xmin)+', '+str(ymin)+', '+str(xmax)+', '+str(ymax)
+    
+    res = []
+    maxWhitePix = 480 * 320 * threshold/100
+
+    for i, (day, j) in enumerate(vidDates):
+        try:
+            r = requests.get(URL, {**params, **{'time': f'{day}/{day}'}}, timeout=10)
+            imgTiff =  np.array(Image.open(io.BytesIO(r.content)))
+            if imgTiff[(imgTiff >= 240).all(axis=2)].shape[0] <= maxWhitePix:
+                res.append((day, imgTiff))
+        except Exception as e:
+            logger.info(f'Download loop, requestst to sentinel-hub, URL: {r.url}. Exception: {e}')
+        if len(res) >= 10:
+            logger.info(f'Download loop, all data downloaded.')
+            return(res)
+            #break
+    
+    '''output is a list with the following structure:
+    [('2018-02-20', array([[[255, 255, 255],
+        [255, 255, 255],
+        ...,]]]))
+    [('2018-02-15', array([[[255, 255, 255],
+        [255, 255, 255],
+        [255, 255, 255],
+        ...,]]]))
+    
+    res[0][1] gives acces to the img data for the first date.
+    res[0][0] gives acces to the first date.'''
+    logger.info(f'Downloaded data for {len(res)} dates.')
+    return(res)
+
+#asdf=0
+while True:
+    
+    #logger.info('outermost while loop')
+    # find oldest file (= oldest request)
+    inDir = 'in/'
+    if os.listdir(inDir) != []:
+        try:
+            #fileID = min([(f, os.path.getctime(os.path.join(inDir,f))) for f in os.listdir(inDir)])[0]
+            fileID = min(os.listdir(inDir), key=lambda f: os.path.getctime(f'{inDir}/{f}'))
+        except Exception as e:
+            logger.info(f'Could not access oldest file: {e}')
+            fileID = None
+    
+    # open file from the /in directory
+    # try, since there might not be any file to begin with
+    if os.listdir(inDir) != []:
+        try:
+            with open(f'in/{fileID}', 'rb') as f:
+                requestedDict = pickle.load(f)
+        except Exception as e:
+            logger.info(f'Failed to open the input file or assign dictionary. {e}')
+            requestedDict = None
+            
+        
+        if requestedDict == None:
+            logger.info(f'empty dictionary.')
+        elif requestedDict['user_id'] == userOld and requestedDict['last_visit'] == timeOld:
+            logger.info(f'Waiting for a new request.')
+        else:
+            logger.info(f'Timelapse request from {requestedDict["user_id"]} at {requestedDict["last_visit"]} for {requestedDict["sat"]} at {requestedDict["location"]}.')
+            start = time.time()
+            sendVid(fileID, requestedDict)
+            end = time.time()
+            duration = end - start
+            logger.info(f'elapsed time to create video: {duration}')
+            
+            userOld = requestedDict['user_id']
+            timeOld = requestedDict['last_visit']
+            
+        
+    time.sleep(5.0)
+
